09e6e178cc52465d83883122fcc84fe4f69d72cc
eaturspinach <markmuskardin@gmail.com>
2012-03-04 13:41:54 -0800

    added credits

--- service/public/javascripts/application.js
+++ service/public/javascripts/application.js
@@ -1,17 +1,17 @@
 Gabulous = {
 	init: function(){
-		var that = this;
+    var that = this;
         var game = new Gabscape();
-		var container = document.getElementById("container");
+    var container = document.getElementById("container");
         game.initialize( { container : container } );
         SB.Graphics.instance.renderer.domElement.focus();
         game.run();
-		TWITTER_CLIENT.getUserTimeLine(that.userTimelineCallback);
-		TWITTER_CLIENT.getUserFriends(that.userFriendsCallback);
+    TWITTER_CLIENT.getUserTimeLine(that.userTimelineCallback);
+    TWITTER_CLIENT.getUserFriends(that.userFriendsCallback);
     TWITTER_CLIENT.getUserData(that.getUserCallback);
     TWITTER_CLIENT.getUserData(that.populateTweetForm);
+    this.postTweet();
 	},
-	
 	userTimelineCallback:function(data)
 	{
 		// console.log("in userTimelineCallback, data = ", data);
@@ -28,8 +28,23 @@ Gabulous = {
   populateTweetForm: function(data){
     $("#twitter_profile_pic").append("<img src="+data[0]["profile_image_url"]+" />");
     $("#twitter_input").append("<h2>"+data[0]["name"]+"</h2>");
-  }
+  },
+  postTweet: function(){
+    $("form#twitter_input_form").submit(function(){
+      alert('gonna submit!');
+      $.ajax({
+        url: '',
+        type: 'POST',
+        success: function(response){
+          alert('successfully tweeted!');
+        },
+        error: function(){
 
+        }
+      });
+      return false;
+    });
+  }
 };
 
 Gabulous.init();

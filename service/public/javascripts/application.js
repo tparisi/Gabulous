7bfdb4e09b3808214d32bcc5f5205d64b87a6b12
Tony Parisi <tparisi@gmail.com>
2012-03-03 20:37:10 -0800

    Integrated 3D test with Gabulous page

--- service/public/javascripts/application.js
+++ service/public/javascripts/application.js
@@ -1,6 +1,11 @@
 Gabulous = {
 	init: function(){
 		var that = this;
+        var game = new Gabscape();
+		var container = document.getElementById("container");
+        game.initialize( { container : container } );
+        SB.Graphics.instance.renderer.domElement.focus();
+        game.run();
 		TWITTER_CLIENT.getUserTimeLine(that.userTimelineCallback);
 		TWITTER_CLIENT.getUserFriends(that.userFriendsCallback);
 	},
@@ -8,11 +13,13 @@ Gabulous = {
 	userTimelineCallback:function(data)
 	{
 		console.log("in userTimelineCallback, data = ", data);
+		this.timelineData = data;
 	},
 	
 	userFriendsCallback:function(data)
 	{
 		console.log("in userFriendsCallback, data = ", data);
+		this.friendsData = data;
 	},
 };
 
